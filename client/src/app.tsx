a14fb7eb6113da0180cb19c3f42dab5adb7cb21c
Kevin Pastor <kevinpastor.mtl@gmail.com>
2022-02-02 11:56:47 -0500

    cleanup

--- client/src/app.tsx
+++ client/src/app.tsx
@@ -1,4 +1,6 @@
-export const App = () => (
+import { ReactElement } from "react";
+
+export const App = (): ReactElement => (
     <h1 className="text-3xl font-bold underline">
         Hello world!
     </h1>
--- client/src/reportWebVitals.ts
+++ client/src/reportWebVitals.ts
@@ -1,15 +1,16 @@
-import { ReportHandler } from 'web-vitals';
+import { ReportHandler } from "web-vitals";
 
-const reportWebVitals = (onPerfEntry?: ReportHandler) => {
-    if (onPerfEntry && onPerfEntry instanceof Function) {
-        import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
+export const reportWebVitals = (onPerfEntry?: ReportHandler) => {
+    if (!onPerfEntry || !(onPerfEntry instanceof Function)) {
+        return
+    }
+
+    import("web-vitals")
+        .then(({ getCLS, getFID, getFCP, getLCP, getTTFB }): void => {
             getCLS(onPerfEntry);
             getFID(onPerfEntry);
             getFCP(onPerfEntry);
             getLCP(onPerfEntry);
             getTTFB(onPerfEntry);
         });
-    }
 };
-
-export default reportWebVitals;
