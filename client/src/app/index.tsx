d0a48c4c983992c7f0776cd6051d894f93a6b8f3
Kevin Pastor <kevinpastor.mtl@gmail.com>
2022-02-02 15:18:31 -0500

    add basic routing

--- client/src/app/index.tsx
+++ client/src/app/index.tsx
@@ -1,7 +1,10 @@
 import { ReactElement } from "react";
+import { Navigate } from "react-router-dom";
 import { BrowserRouter, Route, Routes } from "react-router-dom"
-import { Home } from "./home";
+
 import { Navigation } from "./navigation";
+import { Home } from "./home";
+import { Definition } from "./definition";
 
 export const App = (): ReactElement => (
     <BrowserRouter>
@@ -9,6 +12,8 @@ export const App = (): ReactElement => (
         <main className="container mx-auto py-4">
             <Routes>
                 <Route path="/" element={<Home />} />
+                <Route path="/definitions/:id" element={<Definition />} />
+                <Route path="*" element={<Navigate to="/" />} />
             </Routes>
         </main>
     </BrowserRouter>
