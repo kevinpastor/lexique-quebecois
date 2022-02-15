d07ee50818f54998959360f221b6e3b91425f3f1
Kevin Pastor <kevinpastor.mtl@gmail.com>
2022-02-15 18:02:18 -0500

    paths cleanup

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,3 @@
-//const tsConfig = require("./tsconfig.json");
-//const tsConfigPaths = require("tsconfig-paths");
-//
-//const baseUrl = "./"; // Either absolute or relative path. If relative it's resolved to current working directory.
-//const cleanup = tsConfigPaths.register({
-//    baseUrl,
-//    paths: tsConfig.compilerOptions.paths
-//});
-
 import "reflect-metadata";
 import "dotenv/config";
 
@@ -15,6 +6,3 @@ import { App } from "./app";
 
 const app: App = container.get(App);
 app.start();
-
-//// When path registration is no longer needed
-//cleanup();
