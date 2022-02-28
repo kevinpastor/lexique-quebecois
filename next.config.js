484eec1ebbbc05e60cf9c28c4a751b345688480e
Kevin Pastor <kevinpastor.mtl@gmail.com>
2022-02-28 16:55:05 -0500

    update headers

--- next.config.js
+++ next.config.js
@@ -3,8 +3,13 @@ module.exports = {
     reactStrictMode: true,
     headers: async () => [
         {
-            key: "Content-Security-Policy",
-            value: "default-src 'self' vitals.vercel-insights.com"
+            source: "/:path*",
+            headers: [
+                {
+                    key: "Content-Security-Policy",
+                    value: "default-src 'self' vitals.vercel-insights.com"
+                }
+            ]
         }
     ]
 };
