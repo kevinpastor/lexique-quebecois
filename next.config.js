890c929d4768ac87ac5965fafcc8edbc833be9c0
Kevin Pastor <kevinpastor.mtl@gmail.com>
2022-02-28 17:19:30 -0500

    remove CSP

--- next.config.js
+++ next.config.js
@@ -1,15 +1,4 @@
 /** @type {import("next").NextConfig} */
 module.exports = {
-    reactStrictMode: true,
-    headers: async () => [
-        {
-            source: "/:path*{/}?",
-            headers: [
-                {
-                    key: "Content-Security-Policy",
-                    value: "default-src 'self'; connect-src 'self' vitals.vercel-insights.com;"
-                }
-            ]
-        }
-    ]
+    reactStrictMode: true
 };
