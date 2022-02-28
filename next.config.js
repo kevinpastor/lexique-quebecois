f0a4a3c7cbdfee0255bc1c73058d82147eea5b14
Kevin Pastor <kevinpastor.mtl@gmail.com>
2022-02-28 16:52:55 -0500

    add CSP header

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import("next").NextConfig} */
 module.exports = {
     reactStrictMode: true,
+    headers: async () => [
+        {
+            key: "Content-Security-Policy",
+            value: "default-src 'self' vitals.vercel-insights.com"
+        }
+    ]
 };
