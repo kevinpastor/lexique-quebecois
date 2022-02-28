da8f372c5be13df3667f7e5dc7533a8367bf10ba
Kevin Pastor <kevinpastor.mtl@gmail.com>
2022-02-28 17:15:02 -0500

    update CSP

--- next.config.js
+++ next.config.js
@@ -3,11 +3,11 @@ module.exports = {
     reactStrictMode: true,
     headers: async () => [
         {
-            source: "/:path*",
+            source: "/:path*{/}?",
             headers: [
                 {
                     key: "Content-Security-Policy",
-                    value: "default-src 'self'; connect-src 'self' https://vitals.vercel-insights.com/;"
+                    value: "default-src 'self'; connect-src 'self' vitals.vercel-insights.com;"
                 }
             ]
         }
