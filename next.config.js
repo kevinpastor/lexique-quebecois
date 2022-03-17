7109636eec89a3654cac8a24c6be7a1e87acdda0
Kevin Pastor <kevinpastor.mtl@gmail.com>
2022-03-16 20:14:27 -0400

    add simple pwa setup

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import("next").NextConfig} */
-module.exports = {
-    reactStrictMode: true
-};
+const withPWA = require("next-pwa")
+
+module.exports = withPWA({
+    reactStrictMode: true,
+    pwa: {
+        dest: "public",
+        disable: process.env.NODE_ENV === "development",
+    }
+});
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -20,6 +20,64 @@ const Document = (): ReactElement => (
                 rel="stylesheet"
                 href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&display=swap"
             />
+            <link
+                rel="apple-touch-icon"
+                href="/icons/apple-touch-icon.png"
+            />
+            <link
+                rel="manifest"
+                href="/manifest.json"
+            />
+            <meta
+                name="theme-color"
+                content="#1E293B"
+            />
+            <meta
+                name="application-name"
+                content="Lexique Québécois"
+            />
+            <meta
+                name="apple-mobile-web-app-capable"
+                content="yes"
+            />
+            <meta
+                name="apple-mobile-web-app-status-bar-style"
+                content="default"
+            />
+            <meta
+                name="apple-mobile-web-app-title"
+                content="Lexique Québécois"
+            />
+            <meta
+                name="description"
+                content="Un peu comme Urban Dictionary, mais québécois."
+            />
+            <meta
+                property="og:type"
+                content="website"
+            />
+            <meta
+                property="og:title"
+                content="Lexique Québécois"
+            />
+            <meta
+                property="og:description"
+                content="Un peu comme Urban Dictionary, mais québécois."
+            />
+            <meta
+                property="og:site_name"
+                content="Lexique Québécois"
+            />
+            {/*
+            <meta
+                property="og:url"
+                content="https://yourdomain.com"
+            />
+            <meta
+                property="og:image"
+                content="https://yourdomain.com/icons/apple-touch-icon.png"
+            />
+            */}
         </Head>
         <body>
             <Main />
