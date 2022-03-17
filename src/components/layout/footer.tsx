90136f6eb0d7f9e1b1e02aeede291386afb21309
Kevin Pastor <kevinpastor.mtl@gmail.com>
2022-03-16 23:41:42 -0400

    update footer

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -1,9 +1,16 @@
 import { ReactElement } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCopyright } from "@fortawesome/free-solid-svg-icons";
 
 export const Footer = (): ReactElement => (
     <footer className="container mx-auto">
-        <div className="text-center text-slate-500 font-bold">
-            &copy; 2022 Lexique Québécois
+        <div className="text-center text-slate-500 font-bold space-x-2">
+            <FontAwesomeIcon
+                icon={faCopyright}
+            />
+            <span>
+                2022 Lexique Québécois
+            </span>
         </div>
     </footer>
 );
