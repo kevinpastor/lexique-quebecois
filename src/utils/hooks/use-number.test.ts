3e732e62346a4343a1e8ba387ec46327023f944b
Kevin Pastor <kevinpastor.mtl@gmail.com>
2022-04-20 19:31:08 -0400

    add useNumber tests

---
+++ src/utils/hooks/use-number.test.ts
@@ -0,0 +1,46 @@
+/**
+ * @jest-environment jsdom
+ */
+import { renderHook } from "@testing-library/react-hooks";
+
+import { NumberUtilities, useNumber } from "./use-number";
+
+describe("useNumber", (): void => {
+    it("should initialize", (): void => {
+        const initialValue: number = 0;
+        const { result } = renderHook((): NumberUtilities => useNumber(initialValue));
+
+        expect(result.current.value).toEqual(initialValue);
+    });
+
+    it("should set value", (): void => {
+        const initialValue: number = 0;
+        const { result, rerender } = renderHook((): NumberUtilities => useNumber(initialValue));
+
+        const newValue: number = 1;
+        result.current.setValue(1);
+
+        rerender();
+        expect(result.current.value).toEqual(newValue);
+    });
+
+    it("should increment", (): void => {
+        const initialValue: number = 0;
+        const { result, rerender } = renderHook((): NumberUtilities => useNumber(initialValue));
+
+        result.current.increment();
+
+        rerender();
+        expect(result.current.value).toEqual(initialValue + 1);
+    });
+
+    it("should decrement", (): void => {
+        const initialValue: number = 0;
+        const { result, rerender } = renderHook((): NumberUtilities => useNumber(initialValue));
+
+        result.current.decrement();
+
+        rerender();
+        expect(result.current.value).toEqual(initialValue - 1);
+    });
+});
