fb75674343f4a78044e742c3659823a8e4a801ab
Kevin Pastor <kevinpastor.mtl@gmail.com>
2022-05-18 15:31:03 -0400

    admin hidden test

--- next.config.js
+++ next.config.js
@@ -1,10 +1,36 @@
 /** @type {import("next").NextConfig} */
 const withPWA = require("next-pwa")
+const { PHASE_DEVELOPMENT_SERVER } = require("next/constants");
 
-module.exports = withPWA({
+const pwaConfig = withPWA({
     reactStrictMode: true,
     pwa: {
         dest: "public",
         disable: process.env.NODE_ENV === "development",
     }
 });
+
+const getConfig = (phase, { defaultConfig }) => {
+    const isDevServer = phase === PHASE_DEVELOPMENT_SERVER;
+    
+    /** @type {import('next').NextConfig} */
+    const nextConfig = {
+        // ...defaultConfig,
+        ...pwaConfig,
+        pageExtensions: [
+            "tsx",
+            "ts",
+            "jsx",
+            "js"
+        ].map((extension) => {
+            const prodExtension = `(?<!local\.)${extension}`;
+            const devExtension = `local\.${extension}`;
+            return isDevServer ? [devExtension, extension] : prodExtension;
+        })
+        .flat()
+    }
+    
+    return nextConfig;
+}
+
+module.exports = getConfig 
