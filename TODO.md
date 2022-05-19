3e4bdc765712eb7c8293b7194ec77981a3e5444e
Kevin Pastor <kevinpastor.mtl@gmail.com>
2022-05-19 10:10:10 -0400

    update build script

--- TODO.md
+++ TODO.md
@@ -1,5 +1,6 @@
 # TODO
 
+- Fix copy link button on iOS
 - Add admin page
 - Allow multiple definition per word
 - Add word type (adjective, noun, verb, etc.)
--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@
 const withPWA = require("next-pwa")
 const { PHASE_DEVELOPMENT_SERVER } = require("next/constants");
 
+/** @type {import("next").NextConfig} */
 const pwaConfig = withPWA({
     reactStrictMode: true,
     pwa: {
@@ -10,27 +11,33 @@ const pwaConfig = withPWA({
     }
 });
 
-const getConfig = (phase, { defaultConfig }) => {
+/**
+ * 
+ * @param {string} phase PHASE_EXPORT PHASE_PRODUCTION_BUILD PHASE_PRODUCTION_SERVER PHASE_DEVELOPMENT_SERVER PHASE_TEST
+ * @param {object} options
+ * @param {import("next").NextConfig} options.defaultConfig
+ * @returns {import("next").NextConfig}
+ */
+module.exports = (phase, { defaultConfig }) => {
     const isDevServer = phase === PHASE_DEVELOPMENT_SERVER;
-    
+    const defaultPageExtensions = defaultConfig.pageExtensions ?? [
+        "tsx",
+        "ts",
+        "jsx",
+        "js"
+    ];
+
     /** @type {import('next').NextConfig} */
     const nextConfig = {
         // ...defaultConfig,
         ...pwaConfig,
-        pageExtensions: [
-            "tsx",
-            "ts",
-            "jsx",
-            "js"
-        ].map((extension) => {
+        pageExtensions: defaultPageExtensions.map((extension) => {
             const prodExtension = `(?<!local\.)${extension}`;
             const devExtension = `local\.${extension}`;
             return isDevServer ? [devExtension, extension] : prodExtension;
         })
-        .flat()
+            .flat()
     }
-    
-    return nextConfig;
-}
 
-module.exports = getConfig 
+    return nextConfig;
+}; 
