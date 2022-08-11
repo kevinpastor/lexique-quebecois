6aeb237f6d4da31b3159f5461ca5023c4ca3086b
Kevin Pastor <kevinpastor.mtl@gmail.com>
2022-08-10 22:48:42 -0400

    fix: hydratation test

--- src/components/pages/words-page.tsx
+++ src/components/pages/words-page.tsx
@@ -1,11 +1,10 @@
 /* eslint-disable no-console */
 import { Stack } from "@mui/material";
-import { ReactElement, useEffect } from "react";
+import { ReactElement } from "react";
 import useSWR from "swr";
 
 import { Word as WordComponent } from "@components/misc/word";
 import { Word } from "@models/word";
-import { useBoolean } from "@utils/hooks/use-boolean";
 
 export const WordsPage = (): ReactElement | null => {
     const { data } = useSWR<Array<Word>>("/api/words/sample");
@@ -13,24 +12,9 @@ export const WordsPage = (): ReactElement | null => {
     // `data` coming from `fallback`
     const words: Array<Word> = data as Array<Word>;
 
-    const { value: isClientSide, setTrue } = useBoolean(false);
-
-    useEffect((): void => {
-        setTrue();
-    }, [setTrue]);
-
-    if (!isClientSide) {
-        console.log("I'm on the server.");
-        console.log(words);
-        return null;
-    }
-
-    console.log("I'm on the client.");
-    console.log(words);
-
     return (
         <Stack spacing={2}>
-            {words.map((word: Word): ReactElement => (
+            {[words[0]].map((word: Word): ReactElement => (
                 <WordComponent
                     key={word.timestamp}
                     word={word}
