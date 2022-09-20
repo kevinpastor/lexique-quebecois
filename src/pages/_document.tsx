45a2971fc3a5a4dec04a345ac3b0d527eec17e15
Kevin Pastor <kevinpastor.mtl@gmail.com>
2022-09-20 17:13:44 -0400

    feat: optimization

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -82,7 +82,7 @@ const Document = (): ReactElement => (
             />
             <link
                 rel="stylesheet"
-                href="https://fonts.googleapis.com/css2?family=Lora:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700&display=swap"
+                href="https://fonts.googleapis.com/css2?family=Lora:ital,wght@0,400;0,500;0,600;0,700;1,400&display=swap"
             />
             {metadata.map(({ name, content }: Meta): ReactElement => (
                 <meta
