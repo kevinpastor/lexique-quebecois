3669e3c470d0e7354a86967e3f78b315de56bd32
Kevin Pastor <kevinpastor.mtl@gmail.com>
2022-09-20 21:02:01 -0400

    feat: optimization

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -2,12 +2,11 @@ import { Typography } from "@mui/material";
 import { ReactElement } from "react";
 
 export const Footer = (): ReactElement => (
-    <footer>
-        <Typography
-            align="center"
-            variant="subtitle2"
-        >
-            &copy; 2022 Lexique Québécois
-        </Typography>
-    </footer>
+    <Typography
+        align="center"
+        variant="subtitle2"
+        component="footer"
+    >
+        &copy; 2022 Lexique Québécois
+    </Typography>
 );
