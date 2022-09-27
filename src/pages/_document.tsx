24979b6ad15a051ad2ce8ce79fee40bc0e4ff887
Kevin Pastor <kevinpastor.mtl@gmail.com>
2022-09-27 13:10:46 -0400

    feat: font optimization

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -72,15 +72,6 @@ const Document = (): ReactElement => (
                 href="/manifest.json"
             />
             <link
-                rel="preconnect"
-                href="https://fonts.googleapis.com"
-            />
-            <link
-                rel="preconnect"
-                href="https://fonts.gstatic.com"
-                crossOrigin="anonymous"
-            />
-            <link
                 rel="stylesheet"
                 href="https://fonts.googleapis.com/css2?family=Lora:ital,wght@0,400;0,500;0,600;0,700;1,400&display=swap"
             />
