a3a1f06146b4685f10717465caf77f0b9f15c767
Kevin Pastor <kevinpastor.mtl@gmail.com>
2022-10-06 23:11:02 -0400

    feat: hide ad

--- src/components/layout/sidebar.tsx
+++ src/components/layout/sidebar.tsx
@@ -1,7 +1,7 @@
 import { Card, CardContent, Stack, Typography } from "@mui/material";
 import { ReactElement } from "react";
 
-import { Ad } from "@components/misc/ad";
+// import { Ad } from "@components/misc/ad";
 
 export const Sidebar = (): ReactElement => (
     <Stack
@@ -25,9 +25,12 @@ export const Sidebar = (): ReactElement => (
                 </Typography>
             </CardContent>
         </Card>
+        {/*
+        TODO Show ad when AdSense is approved
         <Ad
             client="ca-pub-3996014859104973"
             slot="4892544775"
         />
+        */}
     </Stack>
 );
