79cc6046f89766d7be1ff9d40a55d7de61d6f366
Kevin Pastor <kevinpastor.mtl@gmail.com>
2023-06-20 10:18:55 -0400

    feat(api): move dislike to app dir

---
+++ src/app/api/words/[id]/dislike/route.ts
@@ -0,0 +1,44 @@
+import { NextRequest, NextResponse } from "next/server";
+
+import { Status } from "@models/status";
+import { dislike, removeDislike } from "@services/api/reactions";
+import { getRequestIp } from "@utils/api/ip";
+import { RateLimiter } from "@utils/api/middlewares/rate-limiter";
+
+const window: number = 1000 * 60 * 15;
+const tokens: number = 100;
+const rateLimiter = new RateLimiter(window, tokens);
+
+interface Params {
+    id: string;
+}
+
+export const PUT = async (request: NextRequest, { params: { id } }: { params: Params }): Promise<NextResponse> => {
+    const ip: string | undefined = getRequestIp(request);
+    if (!ip) {
+        return NextResponse.json(null, { status: Status.Unauthorized });
+    }
+
+    if (rateLimiter.consume(ip)) {
+        return NextResponse.json(null, { status: Status.TooManyRequest });
+    }
+
+    const result: Status = await dislike(id, ip);
+
+    return NextResponse.json(null, { status: result });
+};
+
+export const DELETE = async (request: NextRequest, { params: { id } }: { params: Params }): Promise<NextResponse> => {
+    const ip: string | undefined = getRequestIp(request);
+    if (!ip) {
+        return NextResponse.json(null, { status: Status.Unauthorized });
+    }
+
+    if (rateLimiter.consume(ip)) {
+        return NextResponse.json(null, { status: Status.TooManyRequest });
+    }
+
+    const result: Status = await removeDislike(id, ip);
+
+    return NextResponse.json(null, { status: result });
+};
