bb6945a42f4cdf4b18a14a8e26d6c56ca9ff08d4
Kevin Pastor <kevinpastor.mtl@gmail.com>
2023-06-20 09:18:56 -0400

    feat: improve edge case handling

--- src/app/connexion/page.tsx
+++ src/app/connexion/page.tsx
@@ -6,11 +6,13 @@ import { options } from "src/pages/api/auth/[...nextauth]";
 
 import { LoginPage } from "./component";
 
-const Page = async (): Promise<ReactElement> => {
+const Page = async (): Promise<ReactElement | null> => {
     const session = await getServerSession(options);
 
     if (session) {
         redirect("/");
+
+        return null;
     }
 
     return <LoginPage />;
