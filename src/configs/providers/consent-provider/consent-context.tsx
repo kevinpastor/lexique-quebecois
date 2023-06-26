786d1717f02a50b3ad3f9655b0517d08712f4ae5
Kevin Pastor <kevinpastor.mtl@gmail.com>
2023-06-26 17:32:48 -0400

    feat: refactor consent context

--- src/configs/providers/consent-provider/consent-context.tsx
+++ src/configs/providers/consent-provider/consent-context.tsx
@@ -5,12 +5,12 @@ import { Consent } from "./consent";
 export interface IConsentContext {
     consent: Consent;
 }
-export const ConsentContext = createContext<IConsentContext | null>(null);
+export const ConsentContext = createContext<IConsentContext | undefined>(undefined);
 
 export const useConsent = (): IConsentContext => {
     const context = useContext(ConsentContext);
 
-    if (!context) {
+    if (context === undefined) {
         throw new Error("useConsent must be used within a ConsentProvider");
     }
 
