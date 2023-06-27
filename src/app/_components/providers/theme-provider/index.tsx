8103e9b56e5f8be56368e3a9f86d625dd3e5e5e0
Kevin Pastor <kevinpastor.mtl@gmail.com>
2023-06-26 23:43:11 -0400

    feat: refactor theme provider

--- src/app/_components/providers/theme-provider/index.tsx
+++ src/app/_components/providers/theme-provider/index.tsx
@@ -1,8 +1,9 @@
 import { CssBaseline, Experimental_CssVarsProvider as CssVarsProvider, getInitColorSchemeScript } from "@mui/material";
 import { PropsWithChildren, ReactElement } from "react";
 
-import { theme } from "@configs/theme";
 import { ThemeMode } from "@utils/hooks/use-theme-mode";
+
+import { theme } from "./theme";
 
 const defaultMode: ThemeMode = "system";
 
