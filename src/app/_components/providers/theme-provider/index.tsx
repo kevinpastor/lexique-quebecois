ab7e9dc3a7a80dbbb6dc633aa69418f14a653bfc
Kevin Pastor <kevinpastor.mtl@gmail.com>
2023-06-27 23:37:23 -0400

    fix: add emotion cache provider to fix FOUC

--- src/app/_components/providers/theme-provider/index.tsx
+++ src/app/_components/providers/theme-provider/index.tsx
@@ -1,14 +1,17 @@
 import { CssBaseline, Experimental_CssVarsProvider as CssVarsProvider, getInitColorSchemeScript } from "@mui/material";
 import { PropsWithChildren, ReactElement } from "react";
+import { NextAppDirEmotionCacheProvider, NextAppDirEmotionCacheProviderProps } from "tss-react/next/appDir";
 
 import { ThemeMode } from "@utils/hooks/use-theme-mode";
 
 import { theme } from "./theme";
 
+const options: NextAppDirEmotionCacheProviderProps["options"] = { key: "css" };
 const defaultMode: ThemeMode = "system";
 
 export const ThemeProvider = ({ children }: PropsWithChildren<unknown>): ReactElement => (
-    <>
+    // TODO Replace with the native Emotion cache provider when it will be available for /app.
+    <NextAppDirEmotionCacheProvider options={options}>
         {getInitColorSchemeScript({ defaultMode })}
         <CssVarsProvider
             theme={theme}
@@ -18,5 +21,5 @@ export const ThemeProvider = ({ children }: PropsWithChildren<unknown>): ReactEl
             <CssBaseline enableColorScheme />
             {children}
         </CssVarsProvider>
-    </>
+    </NextAppDirEmotionCacheProvider>
 );
