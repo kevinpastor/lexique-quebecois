62c39fb7afa118a12e349580352e23e82bdee73e
Kevin Pastor <kevinpastor.mtl@gmail.com>
2023-06-28 16:35:39 -0400

    feat: change onClick action to native href

--- src/app/mots/[slug]/_components/missing-word.tsx
+++ src/app/mots/[slug]/_components/missing-word.tsx
@@ -1,36 +1,27 @@
-"use client";
-
 import { Add } from "@mui/icons-material";
 import { Button, Card, CardActions, CardContent, CardHeader, Stack } from "@mui/material";
-import { useRouter } from "next/navigation";
+import Link from "next/link";
 import { ReactElement } from "react";
 
-export const MissingWord = (): ReactElement => {
-    const { push } = useRouter();
-
-    const handleClick = (): void => {
-        push("/ajouter");
-    };
-
-    return (
-        <Card>
-            <CardHeader title="Ce mot n'a pas été trouvé" />
-            <CardContent>
-                Si vous connaissez ce mot, vous pouvez contributer en fournissant une définition et un exemple.
-            </CardContent>
-            <CardActions>
-                <Stack
-                    direction="row-reverse"
-                    width="100%"
+export const MissingWord = (): ReactElement => (
+    <Card>
+        <CardHeader title="Ce mot n'a pas été trouvé" />
+        <CardContent>
+            Si vous connaissez ce mot, vous pouvez contributer en fournissant une définition et un exemple.
+        </CardContent>
+        <CardActions>
+            <Stack
+                direction="row-reverse"
+                width="100%"
+            >
+                <Button
+                    component={Link}
+                    href="/ajouter"
+                    startIcon={<Add />}
                 >
-                    <Button
-                        onClick={handleClick}
-                        startIcon={<Add />}
-                    >
-                        Ajouter
-                    </Button>
-                </Stack>
-            </CardActions>
-        </Card>
-    );
-};
+                    Ajouter
+                </Button>
+            </Stack>
+        </CardActions>
+    </Card>
+);
