95b66844302773dec104374bb28a094503c1d763
Kevin Pastor <kevinpastor.mtl@gmail.com>
2023-07-17 22:56:23 -0400

    feat: update analyze script

--- next.config.js
+++ next.config.js
@@ -3,15 +3,17 @@ const bundleAnalyzer = require("@next/bundle-analyzer");
 
 /** @typedef {(nextConfig?: import("next").NextConfig) => import("next").NextConfig} NextPlugin */
 
+const hasAnalyzeEnvironmentVariable = process.env.ANALYZE === "true";
+
 /** @type {NextPlugin} */
 const withSourceMapAnalyzer = (nextConfig = {}) => ({
     ...nextConfig,
-    productionBrowserSourceMaps: process.env.ANALYZE_SOURCE_MAP === "true",
+    productionBrowserSourceMaps: hasAnalyzeEnvironmentVariable,
 })
 
 /** @type {NextPlugin} */
 const withBundleAnalyzer = bundleAnalyzer({
-    enabled: process.env.ANALYZE_BUNDLE === "true",
+    enabled: hasAnalyzeEnvironmentVariable,
 });
 
 module.exports = withSourceMapAnalyzer(withBundleAnalyzer({
@@ -25,4 +27,10 @@ module.exports = withSourceMapAnalyzer(withBundleAnalyzer({
         }
     },
     reactStrictMode: true,
+    eslint: {
+        ignoreDuringBuilds: hasAnalyzeEnvironmentVariable
+    },
+    typescript: {
+        ignoreBuildErrors: hasAnalyzeEnvironmentVariable
+    }
 }));
