5001ab9777cddfae2a8f0b4e14db0042392c969d
Kevin Pastor <kevinpastor.mtl@gmail.com>
2024-02-01 22:33:58 -0500

    feat: move away from tss-react

--- src/app/_components/providers/theme-provider/index.tsx
+++ src/app/_components/providers/theme-provider/index.tsx
@@ -1,18 +1,17 @@
 import { CssBaseline } from "@mui/material";
 import { Experimental_CssVarsProvider as CssVarsProvider, getInitColorSchemeScript } from "@mui/material/styles";
+import { AppRouterCacheProvider, type AppRouterCacheProviderProps } from "@mui/material-nextjs/v13-appRouter";
 import { type PropsWithChildren, type ReactElement } from "react";
-import { NextAppDirEmotionCacheProvider, type NextAppDirEmotionCacheProviderProps } from "tss-react/next/appDir";
 
 import { type ThemeMode } from "~/hooks/use-theme-mode";
 
 import { theme } from "./theme";
 
-const options: NextAppDirEmotionCacheProviderProps["options"] = { key: "css" };
+const options: AppRouterCacheProviderProps["options"] = { key: "css" };
 const defaultMode: ThemeMode = "system";
 
 export const ThemeProvider = ({ children }: PropsWithChildren): ReactElement => (
-    // TODO Replace with the native Emotion cache provider when it will be available for /app.
-    <NextAppDirEmotionCacheProvider options={options}>
+    <AppRouterCacheProvider options={options}>
         {getInitColorSchemeScript({ defaultMode })}
         <CssVarsProvider
             theme={theme}
@@ -22,5 +21,5 @@ export const ThemeProvider = ({ children }: PropsWithChildren): ReactElement => 
             <CssBaseline enableColorScheme />
             {children}
         </CssVarsProvider>
-    </NextAppDirEmotionCacheProvider>
+    </AppRouterCacheProvider>
 );
