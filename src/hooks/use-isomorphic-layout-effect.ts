961c90e37d22e46ccc486bb8a5359f2501c3c6a6
Kevin Pastor <kevinpastor.mtl@gmail.com>
2024-04-09 23:41:21 -0400

    feat: refactor is server check

--- src/hooks/use-isomorphic-layout-effect.ts
+++ src/hooks/use-isomorphic-layout-effect.ts
@@ -1,8 +1,10 @@
 import { useEffect, useLayoutEffect } from "react";
 
+const IS_SERVER = typeof window === "undefined";
+
 // See: https://usehooks-ts.com/react-hook/use-isomorphic-layout-effect
 export const useIsomorphicLayoutEffect = (
-    typeof window !== "undefined"
-        ? useLayoutEffect
-        : useEffect
+    IS_SERVER
+        ? useEffect
+        : useLayoutEffect
 );
